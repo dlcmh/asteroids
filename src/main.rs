1e71438a7e172da597af40e40948074725be6033
David Chin <dlcmhd@me.com>
2026-05-22 16:00:38 +0800

    rotate and draw ship

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,49 @@
 use macroquad::prelude::*;
 
+const SHIP_HEIGHT: f32 = 25.;
+const SHIP_BASE: f32 = 22.;
+
+#[derive(Debug)]
+struct Ship {
+    pos: Vec2,
+    rot: f32,
+    vel: Vec2,
+}
+
 #[macroquad::main("Asteroids")]
 async fn main() {
+    let mut ship = Ship {
+        pos: Vec2::new(screen_width() / 2., screen_height() / 2.),
+        rot: 0.,
+        vel: Vec2::new(0., 0.),
+    };
     loop {
-        clear_background(DARKPURPLE);
+        clear_background(LIGHTGRAY);
+
+        let rotation = ship.rot.to_radians();
+
+        // Steer ship
+        if is_key_down(KeyCode::Right) {
+            ship.rot += 5.;
+        } else if is_key_down(KeyCode::Left) {
+            ship.rot -= 5.;
+        }
+
+        // Draw ship
+        let v1 = Vec2::new(
+            ship.pos.x + rotation.sin() * SHIP_HEIGHT / 2.,
+            ship.pos.y - rotation.cos() * SHIP_HEIGHT / 2.,
+        );
+        let v2 = Vec2::new(
+            ship.pos.x - rotation.cos() * SHIP_BASE / 2. - rotation.sin() * SHIP_HEIGHT / 2.,
+            ship.pos.y - rotation.sin() * SHIP_BASE / 2. + rotation.cos() * SHIP_HEIGHT / 2.,
+        );
+        let v3 = Vec2::new(
+            ship.pos.x + rotation.cos() * SHIP_BASE / 2. - rotation.sin() * SHIP_HEIGHT / 2.,
+            ship.pos.y + rotation.sin() * SHIP_BASE / 2. + rotation.cos() * SHIP_HEIGHT / 2.,
+        );
+        draw_triangle_lines(v1, v2, v3, 2., BLACK);
+
         next_frame().await
     }
 }
